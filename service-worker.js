e7fa12d6c284f84caf5e5325a4d9017ec159ab13
JagdishNikam <40354033+JagdishNikam@users.noreply.github.com>
2026-07-15 21:40:35 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,5 @@
-const CACHE = "daymark-v15-today-horizontal";
-const ASSETS = [
-  "./", "./index.html", "./styles.css", "./app.js", "./model.js", "./storage.js", "./icons.js",
-  "./manifest.webmanifest", "./icons/icon.svg", "./icons/icon-192.png", "./icons/icon-512.png",
-  "./screenshots/dashboard-mobile.png", "./screenshots/dashboard-wide.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  if (event.request.mode === "navigate") {
-    event.respondWith(fetch(event.request).then(response => {
-      const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put("./index.html",copy)); return response;
-    }).catch(() => caches.match("./index.html")));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(cached => {
-    const network=fetch(event.request).then(response => {
-      if(response.ok && new URL(event.request.url).origin===self.location.origin){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));}
-      return response;
-    }).catch(()=>cached);
-    return cached || network;
-  }));
-});
+const CACHE = "daymark-v2-glass";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { if (event.request.method === "GET") { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); } return response; }).catch(() => caches.match("./index.html")))));
