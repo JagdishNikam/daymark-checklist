8b7ef04158a3a9b68c8cef5e95d2bee75f249b22
JagdishNikam <40354033+JagdishNikam@users.noreply.github.com>
2026-07-16 19:51:10 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "daymark-v29-midnight-neon";
-const VERSION = "20260716-13";
+const CACHE = "daymark-v30-wide-rings";
+const VERSION = "20260716-14";
 const ASSETS = [
   "./", "./index.html", `./styles.css?v=${VERSION}`, `./app.js?v=${VERSION}`, "./model.js", "./storage.js", "./icons.js",
   "./manifest.webmanifest", "./icons/icon.svg", "./icons/icon-192.png", "./icons/icon-512.png",
