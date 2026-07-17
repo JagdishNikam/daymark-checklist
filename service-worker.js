b3cc9b1167d2c12528df6e57af6547c87a021c63
JagdishNikam <40354033+JagdishNikam@users.noreply.github.com>
2026-07-17 16:13:13 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "daymark-v58-dashboard-cell-hover";
-const VERSION = "20260717-16";
+const CACHE = "daymark-v59-today-cyan-focus";
+const VERSION = "20260717-17";
 const ASSETS = [
   "./", "./index.html", `./styles.css?v=${VERSION}`, `./app.js?v=${VERSION}`, "./model.js", "./storage.js", "./icons.js",
   "./manifest.webmanifest", "./icons/icon.svg", "./icons/icon-192.png", "./icons/icon-512.png",
